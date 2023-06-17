7911d126ca3fa05f1f9bda663300acae9abcd1eb
ASHISH KUMAR <83687087+Ashishkumar65@users.noreply.github.com>
2023-06-17 07:51:45 +0530

    Initial commit

---
+++ client/src/pages/Admin/DoctorsList.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import Layout from "../../components/Layout";
+import { showLoading, hideLoading } from "../../redux/alertsSlice";
+import {toast} from 'react-hot-toast'
+import axios from "axios";
+import { Table } from "antd";
+import moment from "moment";
+
+function DoctorsList() {
+  const [doctors, setDoctors] = useState([]);
+  const dispatch = useDispatch();
+  const getDoctorsData = async () => {
+    try {
+      dispatch(showLoading());
+      const resposne = await axios.get("/api/admin/get-all-doctors", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      dispatch(hideLoading());
+      if (resposne.data.success) {
+        setDoctors(resposne.data.data);
+      }
+    } catch (error) {
+      dispatch(hideLoading());
+    }
+  };
+
+  const changeDoctorStatus = async (record, status) => {
+    try {
+      dispatch(showLoading());
+      const resposne = await axios.post(
+        "/api/admin/change-doctor-account-status",
+        { doctorId: record._id, userId: record.userId, status: status },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(hideLoading());
+      if (resposne.data.success) {
+        toast.success(resposne.data.message);
+        getDoctorsData();
+      }
+    } catch (error) {
+      toast.error('Error changing doctor account status');
+      dispatch(hideLoading());
+    }
+  };
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      render: (text, record) => (
+        <span>
+          {record.firstName} {record.lastName}
+        </span>
+      ),
+    },
+    {
+      title: "Specialization",
+      dataIndex: "specialization",
+    },
+    
+    {
+      title: "Phone",
+      dataIndex: "phoneNumber",
+    },
+    {
+      title: "Created At",
+      dataIndex: "createdAt",
+      render: (record , text) => moment(record.createdAt).format("DD-MM-YYYY"),
+    },
+    {
+      title: "status",
+      dataIndex: "status",
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      render: (text, record) => (
+        <div className="d-flex">
+          {record.status === "pending" && (
+          <div className="d-flex">
+          <button
+            className="btn btn-success"
+            onClick={() => changeDoctorStatus(record, "approved")}
+          >
+            Approve
+          </button>
+          <button
+            className="btn btn-danger ms-2"
+            onClick={() => changeDoctorStatus(record, "rejected")}
+          >
+            Reject
+          </button>
+        </div>
+          ) 
+        
+          }
+          {record.status === "approved" && (
+            <button
+              className="btn btn-danger"
+              onClick={() => changeDoctorStatus(record, "blocked")}
+            >
+              Block
+            </button>
+          )}
+        </div>
+      ),
+    },
+  ];
+  return (
+    <Layout>
+      <h1 className="page-header">Doctors List</h1>
+      <hr />
+      <Table columns={columns} dataSource={doctors} />
+    </Layout>
+  );
+}
+
+export default DoctorsList;
